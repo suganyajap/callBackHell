6ba15004027a1a71799a95c4338a538ebcf8a1ec
suganyajap <57367535+suganyajap@users.noreply.github.com>
2021-10-12 12:48:15 +0530

    call back

---
+++ index.js
@@ -0,0 +1,36 @@
+var h2=document.querySelector('h1');
+var n=10;
+setTimeout(()=>{
+    h2.innerText=n--;
+     setTimeout(()=>{
+        h2.innerText=n--;
+        setTimeout(()=>{
+            h2.innerText=n--;
+            setTimeout(()=>{
+                h2.innerText=n--;
+                setTimeout(()=>{
+                    h2.innerText=n--;
+                    setTimeout(()=>{
+                        h2.innerText=n--;
+                        setTimeout(()=>{
+                            h2.innerText=n--;
+                            setTimeout(()=>{
+                                h2.innerText=n--;
+                                setTimeout(()=>{
+                                    h2.innerText=n--;
+                                    setTimeout(()=>{
+                                        h2.innerText=n--;
+                                        setTimeout(()=>{
+                                            h2.innerText=`Happy Independence Day!`;
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+    
+},1000);
